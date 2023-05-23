1a2941f070acba231e7f013c6184a5c82f4bb08b
Lenovo <Lenovo@DESKTOP-JSE715S>
2023-05-23 22:43:22 +0530

    Second commit

---
+++ StringPractice/src/StringRevision/ReverseString.java
@@ -0,0 +1,17 @@
+package StringRevision;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String str="Dipak";
+		
+		char[] ch=str.toCharArray();
+		
+		for(int i=ch.length-1; i>=0 ; i--) {
+			System.out.println(ch[i]);
+		}
+
+	}
+
+}
